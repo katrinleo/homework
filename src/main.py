d754ff60b05ec4183cce00cb4794c73215f7e21b
Katrin <Carpe_diemsk@mail.ru>
2025-03-26 20:16:56 +0300

    fix mypy flake8

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
+
 card_number_str = input("Введите номер карты: ")
 print(get_mask_card_number(card_number_str))
 
--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,13 @@
-def filter_by_state(list_dict: list[dict], state: str = 'EXECUTED'):
-    """функция принимающая список словарей и возвращающая отфильтрованный список словарей"""
-    new_list_dict = []
-    for d in list_dict:
+def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """функция принимающая список словарей и
+    возвращающая отфильтрованный список словарей"""
+    output_data = []
+    for d in data:
         if d["state"] == state:
-            new_list_dict.append(d)
-    return new_list_dict
+            output_data.append(d)
+    return output_data
 
 
-
-def sort_by_date(list_dict: list[dict], parameter: bool = True):
+def sort_by_date(data: list[dict], bool_parameter: bool = True) -> list[dict]:
     """функиия принимает список словарей и сортирует по дате"""
-    return sorted(list_dict, key=lambda x: x["date"], reverse=parameter)
-
+    return sorted(data, key=lambda x: x["date"], reverse=bool_parameter)
