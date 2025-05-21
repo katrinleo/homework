0c7401575268260f40aff3ca40b57213cd294cb4
Katrin <Carpe_diemsk@mail.ru>
2025-05-21 15:57:16 +0300

    fix comments

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,12 @@ def load_json_list(path_to_file: str) -> list[dict | None]:
     """загружает список финансовых транзакци из JSON - файла"""
     if not os.path.exists(path_to_file) or os.path.getsize(path_to_file) == 0:
         return []
-    with open(path_to_file, "r", encoding="utf-8") as file:
-        data = json.load(file)
-        if isinstance(data, list):
-            return data
+    try:
+        with open(path_to_file, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            if isinstance(data, list):
+                return data
+            else:
+                return []
+    except json.JSONDecodeError:
+        return []
